467a875ef8f234325382c5d2d8aa0724c77c6bea
HyuuunjuKim <gus7wn@gmail.com>
2021-10-02 20:50:27 +0900

    feat: next 설정 및 SSR 구현

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SpinButton } from '../src/components';
+
+const Index = () => {
+  return <SpinButton />;
+};
+
+export default Index;
