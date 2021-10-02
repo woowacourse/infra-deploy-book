7b5eb249d8905c8a28b7b2b94b32da62ed4a0d8c
HyuuunjuKim <gus7wn@gmail.com>
2021-10-03 06:31:00 +0900

    refactor: CSR 관련 요소 삭제

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { SpinButton } from '../src/components';
 import Link from 'next/link';
+import PATH from '../src/constants/path';
 
-const Index = () => {
-  return (
-    <>
-      <SpinButton />
+const Index = () => (
+  <>
+    <SpinButton />
 
-      <Link href="/carousel">
-        <a>Carousel 페이지로 가기</a>
-      </Link>
-    </>
-  );
-};
+    <Link href={PATH.CAROUSEL}>
+      <a>Carousel 페이지로 가기</a>
+    </Link>
+  </>
+);
 
 export default Index;
--- src/constants/path.js
+++ src/constants/path.js
@@ -1,7 +1,6 @@
 const PATH = {
   SPIN_BUTTON: '/',
   CAROUSEL: '/carousel',
-  MAIN_NAVIGATION: '/main-navigation',
 };
 
 export default PATH;
