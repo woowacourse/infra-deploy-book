c526d54d5475d7ca0b7abc06db0ec98774b915f2
HyuuunjuKim <gus7wn@gmail.com>
2021-10-03 06:58:39 +0900

    refactor: title, meta 태그 _app.js로 이동

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="엘라의 항공사 웹사이트의 컴포넌트 접근성 높이기 미션" />
+      <title>Ella's airline</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
